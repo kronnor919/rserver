865a90a4916cca0e7c425c0c7890482f059b1065
kronnor919 <segundacuenta7364@gmail.com>
2026-05-15 11:52:35 -0400

    feat: add function to run server
    Process incoming requests, one by one, no multi-threading. Accept -> Parse -> Route -> Response -> Shutdown; and repeat cicle

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 mod http;
 
+use std::{
+    io::{BufReader, Write},
+    net::{Shutdown, TcpListener},
+};
+
 pub use http::{
     headers::{ConnectionHeader, ContentTypeHeader, HttpHeaders},
     methods::HttpMethod,
@@ -8,3 +13,35 @@ pub use http::{
     router::HttpRouter,
     status::{HttpStatus, ParseHttpStatusError},
 };
+
+pub fn run(addr: &str, router: &HttpRouter) {
+    let listener = TcpListener::bind(addr).expect(&format!("Error binding to {addr}"));
+
+    for stream in listener.incoming() {
+        let mut stream = stream.expect("Error accepting client");
+
+        let buffer = BufReader::new(&stream);
+
+        let request = HttpRequest::from(buffer);
+        let route = (request.method().clone(), request.route().to_string());
+
+        let view_function = router.get_route(route);
+
+        let response = match view_function {
+            Some(function) => function(),
+            None => HttpResponse::build(
+                "",
+                HttpStatus::not_found(),
+                HttpHeaders::build(ConnectionHeader::Close, None),
+            ),
+        };
+
+        stream
+            .write_all(response.to_string().as_bytes())
+            .expect("Error writing into buffer");
+
+        stream
+            .shutdown(Shutdown::Both)
+            .expect("Error while shutdown stream");
+    }
+}
